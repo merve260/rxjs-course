c978336afc2dc35d511033cd21b59ec0eddc55a7
Merve Koc <merve260@gmail.com>
2025-05-07 14:54:56 +0200

    features1-07.05.2025

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -29,8 +29,10 @@ export class AboutComponent implements OnInit {
 
     ngOnInit() {
 
-    const interval$ = timer(3000,1000);
-    interval$.subscribe(val => console.log("stream1 => " + val));
+    /* const interval$ = timer(3000,1000);
+    const sub = interval$.subscribe(val => console.log("stream1 => " + val));
+
+    setTimeout(() => sub.unsubscribe(),5000);//sayfa acildiktan 5 saniye sonra saymayi durdurur
     
     const click$ = fromEvent(document, 'click'); //önce tanimlanir
         click$.subscribe(
@@ -38,7 +40,34 @@ export class AboutComponent implements OnInit {
             err => console.log(err), //Hata olursa calisir
             () => console.log("completed") //program biterse calisir ama fromEvent sonsuzdur yani müdahale edilmedigi sürece bitmez
             
-        );
+        ); */
+        // Özel bir Observable yani gözlemci oluşturuyoruz
+        const http$ = Observable.create( observer => {
+            // sen burada observer.next(), observer.error(), observer.complete() çağırabilirsin
+            fetch('/api/courses') // 1. Sunucuya HTTP isteği gönderiyoruz (asenkron çalışır, Promise döner)
+            // 2. Gelen cevabı JSON formatına çeviriyoruz (bu da Promise döner)
+            .then(response => {
+                return response.json(); // burası veri hazır olduğunda çalışır
+            })
+            // 3. JSON verisi geldiğinde gözlemcilere (subscribe edenlere) veriyi iletiyoruz
+            .then(body => {
+                observer.next(body);  // veri gönderildi
+                observer.complete();  // akış tamamlandı bilgisi gönderildi
+            } )
+                // 4. Hata olursa: hem hata loglanıyor hem de gözlemcilere iletiliyor
+                .catch(err => {
+                    observer.error(err);
+                })
+
+                http$.subscribe({
+                    next: data => console.log('Data:', data),  //  next: Observable veri gönderdiğinde bu fonksiyon çalışır
+                    // yani observer.next(...) çağrıldığında burası tetiklenir
+                    error: err => console.error('Fehler:', err), // error: Eğer Observable içinde bir hata oluşursa bu fonksiyon çalışır
+                    // yani observer.error(...) çağrıldığında tetiklenir yani burasi gözlemcinin degil subjectin görebilecegi bir hata mesajidir.
+                    complete: () => console.log('complete') // complete: Observable akışı sona erdiğinde (tamamlandığında) bu fonksiyon çalışır
+                    // yani observer.complete() çağrıldığında tetiklenir
+                  });
+        });
 }
 
 }
